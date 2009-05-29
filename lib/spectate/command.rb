333b857ba212de60ffbefaf09596b21645c3ff44
Stephen Eley <sfeley@gmail.com>
2009-05-29 15:00:10 -0400

    Command line usage display

--- lib/spectate/command.rb
+++ lib/spectate/command.rb
@@ -1,11 +1,19 @@
+require 'optparse'
+
 module Spectate
   
   # Drives the 'spectate' command line utility.
   class Command
+    
     # The primary event called by the command line
     def self.run
-      puts __FILE__
+      options = OptionParser.new
+      options.on("--help", "-?", "--usage", "Displays this help screen") {|o| puts options.to_s}
+      unparsed = options.parse(ARGV)
       true
+    rescue OptionParser::ParseError
+      puts "Oops... #{$!}"
+      puts options
     end
   end
 end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,8 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Command 'spectate'" do
-  it "should execute" do
+  it "executes" do
     system('spectate').should be_true
   end
   
+  # We have several options for displaying help
+  %w{-h -? --help --usage}.each do |option|
+    it "displays its usage when called with #{option}" do
+      `spectate #{option}`.should =~ /Usage:/
+    end
+  end
+  
+  it "displays its usage when given parameters it doesn't understand" do
+    `spectate --blah1234`.should =~ /invalid option: --blah1234.*Usage:/m
+  end
 end
